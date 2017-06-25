4fc1a245161efeebca0d175bb7674f8c5c9b9630
Mario Grieco <mariojosuexz@gmail.com>
2017-06-24 22:09:43 -0400

    ready

--- js/LoadingAnim.js
+++ js/LoadingAnim.js
@@ -1,11 +1,14 @@
-function anim(object){
+var timeCounter = 0;
 
+function anim(object){
+		timeCounter+= 40;
 		var text = object.textContent;
         object.textContent = "";
         var porcentTo     = parseInt(text);
         var porcentFrom   = 0;
         
-        var interval      = setInterval(function() { 
+        setTimeout(function(){
+        	    var interval      = setInterval(function() { 
           	
           	  	if ( porcentFrom + 2 < porcentTo  ) {
             		porcentFrom += 2;
@@ -14,14 +17,15 @@ function anim(object){
             	else {
            	 	  object.textContent = text;
            	 	  clearInterval(interval);
+           	 	  timeCounter-=40;
             	}
 
             }, 20 + parseInt(10 * Math.random()));
+        },timeCounter);
 }
 
 
 $('.skils_type').on('scrollSpy:enter', function() {
-	console.log('enter:', this.children[1]);
 	anim(this.children[1])
 });
 
